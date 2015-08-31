e0ae17736a267eb6f0975f7b8198ad2f991a9a77
Cmdv <vincent@cmdv.org>
2015-08-31 19:57:03 +0100

    creating a component for shallow render

---
+++ src/__test__/router.spec.js
@@ -0,0 +1,7 @@
+import test from 'tape';
+import React from 'react/addons';
+import createComponent from './createComponent_specs';
+import Router from 'react-router';
+
+const Route = Router.Route;
+const TestUtils = React.addons.TestUtils;
